abf74dcb837c5f210a5a17126e59c67a8ba2046a
solairerove <solairerove@gmail.com>
2025-03-06 16:36:11 +0300

    feature: sometimes date can be ok

--- src/main/java/com/github/solairerove/oopt_murmansk/excel/OoptExcelFileParser.java
+++ src/main/java/com/github/solairerove/oopt_murmansk/excel/OoptExcelFileParser.java
@@ -8,10 +8,9 @@ import org.apache.poi.ss.usermodel.Workbook;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
 
+import java.io.FileInputStream;
 import java.io.IOException;
-import java.io.InputStream;
 import java.time.LocalDate;
-import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
@@ -21,30 +20,33 @@ import java.util.Map;
 @Service
 public class OoptExcelFileParser {
 
-    public Map<String, List<VisitPeriod>> parseExcelFile(String fileName) throws IOException {
-        log.info("Чтение файла: {}", fileName);
+    public Map<String, List<VisitPeriod>> parseExcelFile(String filePath) throws IOException {
+        log.info("Чтение файла: {}", filePath);
 
         Map<String, List<VisitPeriod>> visitsByPerson = new HashMap<>();
 
-        try (InputStream inputStream = getClass().getClassLoader().getResourceAsStream(fileName)) {
-            if (inputStream == null) {
-                throw new IllegalArgumentException("Файл не найден: " + fileName);
-            }
-
-            Workbook workbook = new XSSFWorkbook(inputStream);
+        try (FileInputStream fileInputStream = new FileInputStream(filePath)) {
+            Workbook workbook = new XSSFWorkbook(fileInputStream);
             Sheet sheet = workbook.getSheetAt(0);
 
             // Проходим по всем строкам, начиная со второй (первая — заголовки)
             for (int i = 1; i <= sheet.getLastRowNum(); i++) {
                 Row row = sheet.getRow(i);
                 if (row == null) continue;
+                if (row.getCell(0) == null) continue;
+                if (row.getCell(1) == null) continue;
+                if (row.getCell(0).getLocalDateTimeCellValue() == null) continue;
+                if (row.getCell(1).getLocalDateTimeCellValue() == null) continue;
 
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
-                LocalDate entryDate = LocalDate.parse(row.getCell(2).getStringCellValue(), formatter); // Дата въезда
-                LocalDate exitDate = LocalDate.parse(row.getCell(3).getStringCellValue(), formatter); // Дата выезда
+//                DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd.MM.yyyy");
+//                LocalDate entryDate = LocalDate. parse(row.getCell(1).getStringCellValue(), formatter); // Дата въезда
+
+                LocalDate entryDate = row.getCell(0).getLocalDateTimeCellValue().toLocalDate();
+//                LocalDate exitDate = LocalDate.parse(row.getCell(2).getStringCellValue(), formatter); // Дата выезда
+                LocalDate exitDate = row.getCell(1).getLocalDateTimeCellValue().toLocalDate();
                 String visitorName = row.getCell(4).getStringCellValue().trim().replaceAll("\\s+", " "); // ФИО посетителя (убираем лишние пробелы)
-                String blankSent = row.getCell(18).getStringCellValue(); // Бланк отправлен
-                String status = row.getCell(19).getStringCellValue(); // Статус
+                String blankSent = row.getCell(12).getStringCellValue(); // Бланк отправлен
+                String status = row.getCell(13).getStringCellValue(); // Статус
 
                 // Фильтрация данных
                 if ("Да".equalsIgnoreCase(blankSent) && "Выдано".equalsIgnoreCase(status)) {

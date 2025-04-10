bc8efa636b3b1e9fec0af3b9a711db2b2e9f5937
solairerove <solairerove@gmail.com>
2025-04-10 20:34:28 +0800

    feature: iterate over sheets

--- src/main/java/com/github/solairerove/oopt_murmansk/excel/OoptExcelFileParser.java
+++ src/main/java/com/github/solairerove/oopt_murmansk/excel/OoptExcelFileParser.java
@@ -4,11 +4,8 @@ import com.github.solairerove.oopt_murmansk.model.VisitPeriod;
 import lombok.extern.slf4j.Slf4j;
 import org.apache.poi.ss.usermodel.Row;
 import org.apache.poi.ss.usermodel.Sheet;
-import org.apache.poi.ss.usermodel.Workbook;
-import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
 
-import java.io.FileInputStream;
 import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
@@ -25,41 +22,31 @@ public class OoptExcelFileParser {
     private final static String BLANK_SET_FILTER = "Да";
     private final static String STATUS_FILTER = "Выдано";
 
-    public Map<String, List<VisitPeriod>> parseExcelFile(String filePath) throws IOException {
-        log.info("Чтение файла: {}", filePath);
-
+    public Map<String, List<VisitPeriod>> parseExcelFile(Sheet sheet) throws IOException {
         Map<String, List<VisitPeriod>> visitsByPerson = new HashMap<>();
+        // Проходим по всем строкам, начиная со второй (первая — заголовки)
+        for (int i = 1; i <= sheet.getLastRowNum(); i++) {
+            Row row = sheet.getRow(i);
+            if (row == null) continue;
+            if (row.getCell(2) == null) continue;
+            if (row.getCell(3) == null) continue;
+            if (row.getCell(2).getStringCellValue() == null) continue;
+            if (row.getCell(3).getStringCellValue() == null) continue;
 
-        try (FileInputStream fileInputStream = new FileInputStream(filePath)) {
-            Workbook workbook = new XSSFWorkbook(fileInputStream);
-            Sheet sheet = workbook.getSheetAt(0);
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_PATTERN);
+            LocalDate entryDate = LocalDate.parse(row.getCell(2).getStringCellValue(), formatter); // Дата въезда
+            LocalDate exitDate = LocalDate.parse(row.getCell(3).getStringCellValue(), formatter); // Дата выезда
+            String visitorName = row.getCell(4).getStringCellValue().trim().replaceAll("\\s+", " "); // ФИО посетителя (убираем лишние пробелы)
+            String blankSent = row.getCell(18).getStringCellValue(); // Бланк отправлен
+            String status = row.getCell(19).getStringCellValue(); // Статус
 
-            // Проходим по всем строкам, начиная со второй (первая — заголовки)
-            for (int i = 1; i <= sheet.getLastRowNum(); i++) {
-                Row row = sheet.getRow(i);
-                if (row == null) continue;
-                if (row.getCell(2) == null) continue;
-                if (row.getCell(3) == null) continue;
-                if (row.getCell(2).getStringCellValue() == null) continue;
-                if (row.getCell(3).getStringCellValue() == null) continue;
+            // Фильтрация данных
+            if (BLANK_SET_FILTER.equalsIgnoreCase(blankSent) && STATUS_FILTER.equalsIgnoreCase(status)) {
+                VisitPeriod period = new VisitPeriod(entryDate, exitDate);
 
-                DateTimeFormatter formatter = DateTimeFormatter.ofPattern(DATE_PATTERN);
-                LocalDate entryDate = LocalDate. parse(row.getCell(2).getStringCellValue(), formatter); // Дата въезда
-                LocalDate exitDate = LocalDate.parse(row.getCell(3).getStringCellValue(), formatter); // Дата выезда
-                String visitorName = row.getCell(4).getStringCellValue().trim().replaceAll("\\s+", " "); // ФИО посетителя (убираем лишние пробелы)
-                String blankSent = row.getCell(18).getStringCellValue(); // Бланк отправлен
-                String status = row.getCell(19).getStringCellValue(); // Статус
-
-                // Фильтрация данных
-                if (BLANK_SET_FILTER.equalsIgnoreCase(blankSent) && STATUS_FILTER.equalsIgnoreCase(status)) {
-                    VisitPeriod period = new VisitPeriod(entryDate, exitDate);
-
-                    // Добавляем период в список для текущего ФИО
-                    visitsByPerson.computeIfAbsent(visitorName, k -> new ArrayList<>()).add(period);
-                }
+                // Добавляем период в список для текущего ФИО
+                visitsByPerson.computeIfAbsent(visitorName, k -> new ArrayList<>()).add(period);
             }
-
-            workbook.close();
         }
 
         return visitsByPerson;
--- src/main/java/com/github/solairerove/oopt_murmansk/processor/OoptStatisticsProcessor.java
+++ src/main/java/com/github/solairerove/oopt_murmansk/processor/OoptStatisticsProcessor.java
@@ -8,8 +8,12 @@ import com.github.solairerove.oopt_murmansk.statistics.OoptStatisticsLogger;
 import com.github.solairerove.oopt_murmansk.statistics.OoptStatisticsWriter;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.apache.poi.ss.usermodel.Sheet;
+import org.apache.poi.ss.usermodel.Workbook;
+import org.apache.poi.xssf.usermodel.XSSFWorkbook;
 import org.springframework.stereotype.Service;
 
+import java.io.FileInputStream;
 import java.io.IOException;
 import java.util.List;
 import java.util.Map;
@@ -19,8 +23,8 @@ import java.util.Map;
 @RequiredArgsConstructor
 public class OoptStatisticsProcessor {
 
-    private final static String pathToInput = "/Users/solairerove/Downloads/Статистика за февраль, март 2025.xlsx";
-    private final static String pathToOutput = "src/main/resources/output.txt";
+    private final static String PATH_TO_INPUT = "/Users/solairerove/Downloads/Статистика за февраль, март 2025.xlsx";
+    private final static String PATH_TO_OUTPUT = "src/main/resources/%s.txt";
 
     private final OoptExcelFileParser ooptExcelFileParser;
     private final OoptStatisticsAggregator ooptStatisticsAggregator;
@@ -29,11 +33,25 @@ public class OoptStatisticsProcessor {
 
     public void process() {
         try {
-            Map<String, List<VisitPeriod>> visitsByPerson = this.ooptExcelFileParser.parseExcelFile(pathToInput);
-            AggregatedVisits aggregatedVisits = this.ooptStatisticsAggregator.aggregateVisits(visitsByPerson);
-            this.ooptStatisticsLogger.logStatisticsByNames(aggregatedVisits);
-            this.ooptStatisticsLogger.logTotalStatisticsByYearsAndMonths(aggregatedVisits);
-            this.ooptStatisticsWriter.writeToFile(pathToOutput, aggregatedVisits);
+            try (FileInputStream fileInputStream = new FileInputStream(PATH_TO_INPUT)) {
+                Workbook workbook = new XSSFWorkbook(fileInputStream);
+                int numberOfSheets = workbook.getNumberOfSheets();
+                for (int i = 0; i < numberOfSheets; i++) {
+                    Sheet sheet = workbook.getSheetAt(i);
+                    String sheetName = sheet.getSheetName();
+
+                    Map<String, List<VisitPeriod>> visitsByPerson = this.ooptExcelFileParser.parseExcelFile(sheet);
+                    AggregatedVisits aggregatedVisits = this.ooptStatisticsAggregator.aggregateVisits(visitsByPerson);
+                    this.ooptStatisticsLogger.logStatisticsByNames(aggregatedVisits);
+                    this.ooptStatisticsLogger.logTotalStatisticsByYearsAndMonths(aggregatedVisits);
+
+                    var outputFile = String.format(PATH_TO_OUTPUT, sheetName);
+                    this.ooptStatisticsWriter.writeToFile(outputFile, aggregatedVisits);
+                }
+
+                workbook.close();
+            }
+
         } catch (IOException e) {
             log.error("Ошибка обработки файла", e);
         }
